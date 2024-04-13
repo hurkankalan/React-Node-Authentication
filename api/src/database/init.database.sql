8062c2a6e015272cdeb10bc993401c6bfc0c0e8e
hurkankalan <hurkan.kalan@gmail.com>
2024-04-13 13:58:36 +0200

    created type for query result in usersModels functions and update user table column

--- api/src/database/init.database.sql
+++ api/src/database/init.database.sql
@@ -8,10 +8,14 @@ CREATE DATABASE auth_app;
 
 \c auth_app;
 
+CREATE TYPE role AS ENUM ('admin', 'user');
+
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    role role DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- api/src/models/users.model.ts
+++ api/src/models/users.model.ts
@@ -1,33 +1,39 @@
 import pool from "../database/config.database";
+import { QueryResult } from "pg";
+import { Users } from "../types/users.type";
 
 const usersModels = {
-  getAllUsers: () => {
+  getAllUsers(): Promise<QueryResult<Users>> {
     return pool.query("SELECT * FROM users");
   },
 
-  getUserById: (id: number) => {
+  getUserById(id: number): Promise<QueryResult<Users>> {
     return pool.query("SELECT * FROM users WHERE id = $1", [id]);
   },
 
-  getUserByEmail: (email: string) => {
+  getUserByEmail(email: string): Promise<QueryResult<Users>> {
     return pool.query("SELECT * FROM users WHERE email = $1", [email]);
   },
 
-  createUser: (email: string, password: string) => {
+  createUser(email: string, password: string): Promise<QueryResult<Users>> {
     return pool.query("INSERT INTO users (email, password) VALUES ($1, $2)", [
       email,
       password,
     ]);
   },
 
-  updateUser: (id: number, email: string, password: string) => {
+  updateUser(
+    id: number,
+    email: string,
+    password: string
+  ): Promise<QueryResult<Users>> {
     return pool.query(
       "UPDATE users SET email = $2, password = $3 WHERE id = $1",
       [id, email, password]
     );
   },
 
-  deleteUser: (id: number) => {
+  deleteUser(id: number): Promise<QueryResult<Users>> {
     return pool.query("DELETE FROM users WHERE id = $1", [id]);
   },
 };
