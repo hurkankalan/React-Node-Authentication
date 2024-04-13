816a2272d7e1e0a19caa3553a073f955d2c6eb29
hurkankalan <hurkan.kalan@gmail.com>
2024-04-13 16:10:56 +0200

    create login and register functions

--- api/src/controllers/users.controller.ts
+++ api/src/controllers/users.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { comparePassword } from "../utils/comparePassword";
+import { hashPassword } from "../utils/hashPassword";
 import usersModels from "../models/users.model";
 import { Users } from "../types/users.type";
 
@@ -122,6 +124,83 @@ const usersControllers = {
       }
 
       return res.sendStatus(204);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  },
+
+  async register(req: Request, res: Response): Promise<Response> {
+    const { email, password } = req.body;
+
+    for (const key in req.body) {
+      if (!req.body[key]) {
+        res
+          .status(400)
+          .json({ error: "One or more data are missing in the body" });
+      }
+    }
+
+    try {
+      const user = await usersModels.getUserByEmail(email);
+
+      if (user.rows[0]) {
+        return res.status(409).json("User already exists");
+      }
+
+      const hashedPassword = await hashPassword(password);
+
+      const newUser = await usersModels.createUser(email, hashedPassword);
+
+      if (newUser.rowCount === 0) {
+        return res.status(500).json("User isn't created");
+      }
+
+      return res.sendStatus(201);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  },
+
+  async login(req: Request, res: Response): Promise<Response> {
+    const { email, password } = req.body;
+
+    for (const key in req.body) {
+      if (!req.body[key]) {
+        res
+          .status(400)
+          .json({ error: "One or more data are missing in the body" });
+      }
+    }
+
+    try {
+      const user = await usersModels.getUserByEmail(email);
+
+      if (!user.rows[0]) {
+        return res.status(404).json("User not found");
+      }
+
+      const passwordIsValid = await comparePassword(
+        password,
+        user.rows[0].password
+      );
+
+      if (!passwordIsValid) {
+        return res.status(401).json("Password is incorrect");
+      }
+
+      if (!process.env.JWT_SECRET) {
+        return res.status(404).json({ error: "JWT_SECRET is missing" });
+      }
+
+      const token = jwt.sign(
+        { id: user.rows[0].id, email: user.rows[0].email },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "24h",
+        }
+      );
+
+      return res.status(200).json(token);
     } catch (error) {
       return res.status(500).json(error);
     }
